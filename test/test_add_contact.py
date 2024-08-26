7108d5af41ce2ed327163fe96f60b00099181fae
v.chernova <wecan-doit@yandex.ru>
2024-08-26 23:31:58 +0300

    Выделен класс для управления сессиями

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -10,10 +10,10 @@ def app(request):
     return fixture
     
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.open_new_contact_form()
     app.create_contact(Contact(firstname="ghgfhg", middlename="jhjhjhj", lastname="jhjjgjhg", nickname="jghjjhggh", title="jghgjjhjgh", company="jjhghjjh", address="jghgjhgjhgjh",
                             home_phone="676767", mobile_phone="676767", work_phone="766767", fax="6776767", email="fggfgff", email2="gfgfgffg", email3="gffgffg", homepage="gfgffggfgf",
                             bday="29", bmonth="September", byear="1989", aday="19", amonth="February", ayear="1989"))
     app.return_homepage()
-    app.logout()
+    app.session.logout()
