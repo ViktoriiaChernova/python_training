f4714745976834093aa15e623ff4bf454fe29a08
v.chernova <wecan-doit@yandex.ru>
2024-08-29 22:57:46 +0300

    Выделен класс для работы с группами

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -11,9 +11,9 @@ def app(request):
     
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
-    app.open_new_contact_form()
-    app.create_contact(Contact(firstname="ghgfhg", middlename="jhjhjhj", lastname="jhjjgjhg", nickname="jghjjhggh", title="jghgjjhjgh", company="jjhghjjh", address="jghgjhgjhgjh",
-                            home_phone="676767", mobile_phone="676767", work_phone="766767", fax="6776767", email="fggfgff", email2="gfgfgffg", email3="gffgffg", homepage="gfgffggfgf",
-                            bday="29", bmonth="September", byear="1989", aday="19", amonth="February", ayear="1989"))
-    app.return_homepage()
+    app.contact.open_new_contact_form()
+    app.contact.create(Contact(firstname="ghgfhg", middlename="jhjhjhj", lastname="jhjjgjhg", nickname="jghjjhggh", title="jghgjjhjgh", company="jjhghjjh", address="jghgjhgjhgjh",
+                               home_phone="676767", mobile_phone="676767", work_phone="766767", fax="6776767", email="fggfgff", email2="gfgfgffg", email3="gffgffg", homepage="gfgffggfgf",
+                               bday="29", bmonth="September", byear="1989", aday="19", amonth="February", ayear="1989"))
+    app.contact.return_homepage()
     app.session.logout()
