6b8cd6ab27fe675c86d428a7afc3577693170c26
v.chernova <wecan-doit@yandex.ru>
2024-09-08 19:33:47 +0300

    Задание 7, работа над ошибками, третья попытка

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -1,7 +1,6 @@
 from model.contact import Contact
 def test_edit_contact(app):
-    app.contact.open_new_contact_form()
-    app.contact.create(Contact(firstname="edited", middlename="edited", lastname="edited", nickname="edited", title="edited", company="edited", address="edited",
+    app.contact.edit(Contact(firstname="edited", middlename="edited", lastname="edited", nickname="edited", title="edited", company="edited", address="edited",
                                home_phone="edited", mobile_phone="edited", work_phone="edited", fax="edited", email="edited", email2="edited", email3="edited", homepage="edited",
                                bday="29", bmonth="September", byear="1989", aday="19", amonth="February", ayear="1989"))
     app.contact.return_homepage()
