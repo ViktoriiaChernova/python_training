ca33511e71c52d3064462626f8feb2c16e4d683e
v.chernova <wecan-doit@yandex.ru>
2024-09-16 22:00:54 +0300

    Загружение списков для тестов групп

--- test/test_edit_first_group.py
+++ test/test_edit_first_group.py
@@ -3,14 +3,23 @@ from model.group import Group
 def test_edit_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(name="edited", header="edited", footer="edited"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 def test_edit_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(name="edited"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 def test_edit_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(header="edited"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
