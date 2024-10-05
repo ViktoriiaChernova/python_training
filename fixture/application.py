77cef17c814fa99bee5e621a7e5ab6380beb4804
v.chernova <wecan-doit@yandex.ru>
2024-10-05 21:55:54 +0300

    Задание №16: Реализованы опции командной строки

--- fixture/application.py
+++ fixture/application.py
@@ -1,16 +1,22 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
 class Application:
-    def __init__(self):
-        self.wd = webdriver.Firefox()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "safari":
+            self.wd = webdriver.Safari()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.wd.implicitly_wait(30)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -21,7 +27,7 @@ class Application:
 
     def open_home_page(self):
             wd = self.wd
-            wd.get("http://localhost/addressbook/index.php")
+            wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
