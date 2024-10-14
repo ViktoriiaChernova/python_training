dbe02a5c2af58581123705cb04bebd0460325cc6
v.chernova <wecan-doit@yandex.ru>
2024-10-14 22:43:33 +0300

    Задание №22: Исправлен тест для добавления контакта в группу

--- test/test_add_contact_to_group.py
+++ test/test_add_contact_to_group.py
@@ -12,12 +12,19 @@ def test_add_contact_to_group(app, db):
                                bday="29", bmonth="September", byear="1989", aday="19", amonth="February", ayear="1989"))
     if len(db.get_group_list()) == 0:
         app.group.create(Group(name="test"))
-    # добавить проверку что в выбранной группе нет выбранного контакта (?добавить удаление или выбрать другую группу?
-    contact = random.choice(db.get_contact_list())
-    c_id = contact.id
     g_id = random.choice(db.get_group_list()).id
+    contacts_not_in_group = db.get_contacts_not_in_group(Group(id=g_id))
+    if len (contacts_not_in_group) == 0:
+        app.contact.open_new_contact_form()
+        app.contact.create(Contact(firstname="ghgfhg", middlename="jhjhjhj", lastname="jhjjgjhg", nickname="jghjjhggh",
+                                   title="jghgjjhjgh", company="jjhghjjh", address="jghgjhgjhgjh",
+                                   home_phone="676767", mobile_phone="676767", work_phone="766767", fax="6776767",
+                                   email="fggfgff", email2="gfgfgffg", email3="gffgffg", homepage="gfgffggfgf",
+                                   bday="29", bmonth="September", byear="1989", aday="19", amonth="February",
+                                   ayear="1989"))
+    contact = random.choice(db.get_contacts_not_in_group(Group(id=g_id)))
     contacts_in_group_before_adding = db.get_contacts_in_group(Group(id=g_id))
-    app.contact.add_contact_to_group_by_id(c_id, g_id)
+    app.contact.add_contact_to_group_by_id(contact.id, g_id)
     contacts_in_group_after_adding = db.get_contacts_in_group(Group(id=g_id))
     contacts_in_group_before_adding.append(contact)
     assert sorted(contacts_in_group_before_adding, key=Contact.id_or_max) == sorted(contacts_in_group_after_adding, key=Contact.id_or_max)
